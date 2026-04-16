79b739725e8341e49a244e2f2ecc8d0d0a78710e
mepuka <kokokessy@gmail.com>
2026-04-15 19:59:34 -0500

    remove analysis and dead code (#130)

--- src/client/index.ts
+++
@@ -1,2 +0,0 @@
-export { makePublicReadClient, type PublicReadClient } from "./PublicReadClient";
-export { PublicReadApi } from "../api/PublicReadApi";
--- src/query/Layer.ts
+++
@@ -1,1 +0,0 @@
-export { makeQueryLayer } from "../edge/Layer";
